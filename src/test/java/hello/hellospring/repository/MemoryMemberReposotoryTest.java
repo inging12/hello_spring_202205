38b36af28d42dc85e942fbf53d57f98e772c585c
inging12 <inging12@naver.com>
2022-05-23 15:43:20 +0900

    220623 spring db

--- src/test/java/hello/hellospring/repository/MemoryMemberReposotoryTest.java
+++ src/test/java/hello/hellospring/repository/MemoryMemberReposotoryTest.java
@@ -32,6 +32,24 @@ class MemoryMemberReposotoryTest {
     }
 
     @Test
+    public void findByName(){
+        //given
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        //when
+        Member result = repository.findByName("spring1").get();
+
+        //then
+        assertThat(result).isEqualTo(member1);
+    }
+
+    @Test
     public void findAll(){
         //given
         Member member1 = new Member();
